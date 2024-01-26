8f6ac51eb3c0bfada50bdd69dde902d24e4402f6
hambalee <18182920+hambalee@users.noreply.github.com>
2024-01-26 07:22:59 +0700

    add packages ต่างๆ ที่จำเป็น และ แก้ไฟล์ program.cs เพิ่ม graphql ให้ใช้งานได้แล้ว

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
 var app = builder.Build();
+
+app.MapGraphQL();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
